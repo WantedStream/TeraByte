607356d44b53cc34c30cb415f88e2c2729909aeb
WantedStream <48957819+WantedStream@users.noreply.github.com>
2026-09-03 21:27:54 +0300

    move game

---
+++ thera-byte/player/CharacterBody2D.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+@export var gravity: float = 300.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("The script is attached and alive!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+func _physics_process(delta):
+		var direction=Input.get_vector("left","right","up","down")
+		velocity.x=direction.x*speed
+		velocity.y += gravity* delta
+		print("Current Velocity: ", velocity)
+		move_and_slide()
+		
