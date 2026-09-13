91ffeffc313110e069a11d2f460211e9f4a8081d
WantedStream <48957819+WantedStream@users.noreply.github.com>
2026-09-13 11:38:20 +0300

    tilemap and tileset creat

--- thera-byte/player/CharacterBody2D.gd
+++ thera-byte/player/CharacterBody2D.gd
@@ -2,10 +2,18 @@ extends CharacterBody2D
 
 @export var speed: float = 300.0
 @export var gravity: float = 300.0
-@export var health = 5
+
+@export var max_health: int = 100
+var current_health: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("The script is attached and alive!")
+	
+	current_health = max_health
+	
+	# 2. Wire the interrupt to the Event Bus
+	EventBus.player_damaged.connect(take_damage)
 	pass # Replace with function body.
 
 
@@ -13,7 +21,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
+func take_damage(amount: int):
+	# Subtract the damage
+	current_health -= amount
+	
+	# Let's print it to prove the math works
+	print("Ouch! Took ", amount, " damage. Health is now: ", current_health)
+	
+	# Check if the player should be dead
+	if current_health <= 0:
+		current_health = 0 # Prevent negative numbers
+		die()
 
+func die():
+	print("THE PLAYER HAS REACHED 0 HEALTH!")
+	
+	# We will build the actual death logic (like switching to a Death state) later.
 #func _physics_process(delta):
 		#var direction=Input.get_vector("left","right","up","down")
 		#velocity.x=direction.x*speed
